2c8323f7c29ba8b182790f021644d15b6f47d8cc
lujwa <407845252@qq.com>
2015-12-28 15:46:28 +0800

    lujwq 12.18

--- dist/js/router.js
+++ dist/js/router.js
@@ -1,5 +1,5 @@
 /**
- * Created by Administrator on 15-12-25.
+ * Created by lujwa on 15-12-25.
  */
 'use strict';
 
@@ -21,6 +21,35 @@ define([
         'myApp.index2'
     ]).
     config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/doc_start', {
+            templateUrl: 'document/index.html'
+            //controller: 'View1Ctrl'
+            //resolve: {
+            //    delay: function($q) {
+            //        var delay = $q.defer(),
+            //            load = function(){
+            //                $.getScript('dist/js/pages/dashboard.js',function(){
+            //                    delay.resolve();
+            //                });
+            //            };
+            //        load();
+            //        return delay.promise;
+            //    }
+            //
+            //}
+        });
+        $routeProvider.when('/layout/boxed', {
+            templateUrl: 'pages/layout/boxed.html'
+        });
+        $routeProvider.when('/layout/fixed', {
+            templateUrl: 'pages/layout/fixed.html'
+        });
+        $routeProvider.when('/layout/collapsed-sidebar', {
+            templateUrl: 'pages/layout/collapsed-sidebar.html'
+        });
+        $routeProvider.when('/widgets', {
+            templateUrl: 'pages/widgets.html'
+        });
         $routeProvider.otherwise({redirectTo: '/index2'});
     }]);
 });
